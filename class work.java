48c0a37cbe1d77658d0c1c59ab864c1ac0d00709
KFry101 <katiefrymire@gmail.com>
2023-08-22 16:14:35 -0500

    hdjsf

---
+++ class work.java
@@ -0,0 +1,8 @@
+/**
+* This is my first Java program. It just prints one line.
+*/
+public class FirstJavaProgram {
+public static void main (String[] args) {
+System.out.println (" This is my first Java program");
+}
+}
